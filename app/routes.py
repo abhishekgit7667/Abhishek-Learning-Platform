121fbb5b7d3cc12cea87a2403b6c879ae200b8ca
Abhishek Singh <abhishekgit7667@gmail.com>
2026-03-23 04:11:39 +0530

    Added Flask app structure, blueprint setup, and fixed templates issue

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,27 @@ main = Blueprint('main', __name__)
 @main.route("/")
 def home():
     return render_template("home.html")
+
+@main.route("/")
+def about():
+    return render_template("about.html")
+
+@main.route("/")
+def base():
+    return render_template("base.html")
+
+@main.route("/")
+def contact():
+    return render_template("contact.html")
+
+@main.route("/")
+def learn():
+    return render_template("learn.html")
+
+@main.route("/")
+def login():
+    return render_template("login.html")
+
+@main.route("/")
+def project():
+    return render_template("project.html")
